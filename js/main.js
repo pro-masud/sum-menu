6acaeeb90c5214547c2eedc51c080397121f00ff
pro-mannan <promasudbd@gmail.com>
2023-08-26 12:17:58 +0600

    complete sub-menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // get all elements here 
 const closeBtn = document.querySelector(".menuToggle");
 const navBar = document.querySelector("nav");
+const menu = document.querySelectorAll("nav ul");
 const menuLis = document.querySelectorAll("nav ul li");
+const menuLisLI = document.querySelector("nav ul > li.active");
 const subMenuClass = document.querySelector("nav ul li ul");
 const supmenu = document.querySelectorAll("nav ul li ul li");
 const topmenu = document.querySelector("nav ul li ul li ul");
@@ -9,6 +11,7 @@ const topmenu = document.querySelector("nav ul li ul li ul");
 // close btn event handling 
 closeBtn.addEventListener("click", () => {
     navBar.classList.toggle("navbar");
+    closeBtn.classList.toggle("close");
 });
 
 
@@ -30,6 +33,3 @@ menuLis.forEach((item) => {
    });
 });
 
-// supmenu.addEventListener("click", () => {
-//     topmenu.style.display = "block";
-// });
